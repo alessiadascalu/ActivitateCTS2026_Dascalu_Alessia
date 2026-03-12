a34dfb737aef8c1345645937c255db2f82c479bd
alessiadascalu <dascalualessia04@gmail.com>
2026-03-12 15:39:23 +0200

    semianr 4

---
+++ seminar4/src/ro/ase/cts/singleton/main/Main.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.singleton.main;
+
+import ro.ase.cts.singleton.clase.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+       Logger instance = Logger.getInstanta("Interfata", "afisare");
+       instance.afiseazaLog("Fisierul JSON nu e afisat corect!");
+       instance.afiseazaLog("eroare");
+       Logger instance2 = Logger.getInstanta("Backend", "baza de date");
+       instance2.afiseazaLog("eroare");
+       instance2.setCategorie("baza de date");
+       instance2.setSender("Backend");
+       instance2.afiseazaLog("eroare la inserare");
+
+    }
+}
